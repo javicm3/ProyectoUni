53143852841ac910e461f6cf5054b0138afd2ba9
juliopm99 <55432277+juliopm99@users.noreply.github.com>
2020-12-05 18:05:05 +0100

    camera de momento, camaras grupales. No estan puestas en la escena

---
+++ Assets/PruebasNuevoMovimiento/Scripts/Camara/AñadirQuitarObjetos.cs
@@ -0,0 +1,112 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AñadirQuitarObjetos : MonoBehaviour
+{
+    CinemachineVirtualCamera cinemakina;
+     CinemachineTargetGroup targetGroup;
+    public bool entrada;
+    public bool salida;
+    public bool dejarSoloPlayer;
+    public GameObject[] objAñadir;
+    public GameObject[] objQuitar;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cinemakina = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
+        targetGroup = GameObject.FindObjectOfType<CinemachineTargetGroup>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    
+    void AñadirObj()
+    {
+      foreach(GameObject go in objAñadir)
+        {
+            for (int i = 0; i < targetGroup.m_Targets.Length; i++)
+            {
+                if (i == 0) {  }
+                else
+                {
+                    if (targetGroup.m_Targets[i].target != null)
+                    {
+
+                        if (targetGroup.m_Targets[i].target == go.transform)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        targetGroup.m_Targets[i].target = go.transform;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    void QuitarObj()
+    {
+        foreach (GameObject go in objQuitar)
+        {
+            for (int i = 0; i < targetGroup.m_Targets.Length; i++)
+            {
+                if (i == 0) { }
+                else
+                {
+                    if (targetGroup.m_Targets[i].target != null)
+                    {
+                        if ( targetGroup.m_Targets[i].target == go.transform)
+                        {
+                            targetGroup.m_Targets[i].target = null;
+                            break;
+                        }
+
+                    }
+                    else
+                    {
+                      
+                    }
+                }
+            }
+        }
+    }
+    void DejarSoloPlayer()
+    {
+        for (int i = 0; i < targetGroup.m_Targets.Length; i++)
+        {
+            if (i == 0) { targetGroup.m_Targets[0].target = player.transform; }
+            else
+            {if (targetGroup.m_Targets[i].target != null)
+                {
+                targetGroup.m_Targets[i].target = null;
+
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (entrada)
+            {
+                AñadirObj();
+            }
+            if (salida)
+            {
+                QuitarObj();
+
+            }
+            if (dejarSoloPlayer) DejarSoloPlayer();
+        }
+    }
+}
--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CameraZoom : MonoBehaviour
 {
     CinemachineVirtualCamera cinemakina;
-   public float startsize = 13;
+    public float startsize = 13;
     public float finalsize = 19;
     ControllerPersonaje cc;
     public GameObject segundorb;
@@ -14,7 +14,11 @@ public class CameraZoom : MonoBehaviour
     float tiempoSinInput;
     public GameObject hud;
     float indiceHUD;
-
+    public GameObject targetGroup;
+    public float maxZoom;
+    public float minZoom;
+    float maxDistance;
+    public float indiceMultiplicadorZoom = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,27 +26,70 @@ public class CameraZoom : MonoBehaviour
         cinemakina = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
         cc = GameObject.FindObjectOfType<ControllerPersonaje>();
     }
+    float DistanciaMaxima()
+    {
+        var bounds = new Bounds(targetGroup.GetComponent<CinemachineTargetGroup>().m_Targets[0].target.position, Vector3.zero);
 
+        for (int i = 0; i < (targetGroup.GetComponent<CinemachineTargetGroup>().m_Targets.Length); i++)
+        {
+            if (targetGroup.GetComponent<CinemachineTargetGroup>().m_Targets[i].target != null) bounds.Encapsulate(targetGroup.GetComponent<CinemachineTargetGroup>().m_Targets[i].target.position);
+        }
+        if (bounds.size.x > bounds.size.y)
+        {
+ return bounds.size.x;
+        }
+        else
+        {
+            return bounds.size.y;
+        }
+       
+    }
     // Update is called once per frame
     void Update()
     {
         //if (GameManager.Instance.personajevivo == true)
         //{
-
-        if (Mathf.Abs(cc.rb.velocity.x) > 7)
+        bool soloplayer = false;
+        for (int i = 0; i < targetGroup.GetComponent<CinemachineTargetGroup>().m_Targets.Length; i++)
         {
-            tiempoSinInput = 0;
-            if (cinemakina.m_Lens.OrthographicSize < finalsize)
+            if (i == 0)
             {
-                cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.x) * indiceMultiplicador * Mathf.Clamp(cc.rb.velocity.x, 1, 1.3f) * Time.deltaTime, 0, 7);
-                
+                if (targetGroup.GetComponent<CinemachineTargetGroup>().m_Targets[0].target == this.gameObject.transform)
+                {
+                    soloplayer = true;
+                }
+                else
+                {
+                    soloplayer = false;
+                }
+            }
+            else
+            {
+                if (targetGroup.GetComponent<CinemachineTargetGroup>().m_Targets[i].target !=null)
+                {
+                    soloplayer = false;
+
+                }
+            }
+        }
+        if (soloplayer==true)
+        {
+
+
+            if (Mathf.Abs(cc.rb.velocity.x) > 7)
+            {
+                tiempoSinInput = 0;
+                if (cinemakina.m_Lens.OrthographicSize < finalsize)
+                {
+                    cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.x) * indiceMultiplicador * Mathf.Clamp(cc.rb.velocity.x, 1, 1.3f) * Time.deltaTime, 0, 7);
+
+                }
+
             }
 
-        }
-        
-        else  if (Mathf.Abs(cc.rb.velocity.x) > 12)
+            else if (Mathf.Abs(cc.rb.velocity.x) > 12)
             {
-            tiempoSinInput = 0;
+                tiempoSinInput = 0;
                 if (cinemakina.m_Lens.OrthographicSize < finalsize)
                 {
                     cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.x) * indiceMultiplicador * Mathf.Clamp(cc.rb.velocity.x, 1, 2f) * Time.deltaTime, 0, 7);
@@ -50,37 +97,37 @@ public class CameraZoom : MonoBehaviour
 
             }
             else if (Mathf.Abs(cc.rb.velocity.x) > 25)
-        {
-            tiempoSinInput = 0;
-            if (cinemakina.m_Lens.OrthographicSize < finalsize)
+            {
+                tiempoSinInput = 0;
+                if (cinemakina.m_Lens.OrthographicSize < finalsize)
                 {
-                    cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.x) * indiceMultiplicador * Mathf.Clamp(cc.rb.velocity.x, 1.5f,2.2f) * Time.deltaTime, 0, 7);
+                    cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.x) * indiceMultiplicador * Mathf.Clamp(cc.rb.velocity.x, 1.5f, 2.2f) * Time.deltaTime, 0, 7);
                 }
 
             }
             else if (Mathf.Abs(cc.rb.velocity.y) > 23f)
-        {
-            tiempoSinInput = 0;
-            if (cinemakina.m_Lens.OrthographicSize < finalsize)
+            {
+                tiempoSinInput = 0;
+                if (cinemakina.m_Lens.OrthographicSize < finalsize)
                 {
-                    cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.y) * indiceMultiplicador *1f * Time.deltaTime, 0, 7);
+                    cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.y) * indiceMultiplicador * 1f * Time.deltaTime, 0, 7);
                 }
 
             }
 
             else if (Mathf.Abs(cc.rb.velocity.y) > 16f)
-        {
-            tiempoSinInput = 0;
-            if (cinemakina.m_Lens.OrthographicSize < finalsize)
+            {
+                tiempoSinInput = 0;
+                if (cinemakina.m_Lens.OrthographicSize < finalsize)
                 {
                     cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.y) * indiceMultiplicador * 0.4f * Time.deltaTime, 0, 7);
                 }
 
             }
             else if (Mathf.Abs(cc.rb.velocity.y) > 8f)
-        {
-            tiempoSinInput = 0;
-            if (cinemakina.m_Lens.OrthographicSize < finalsize)
+            {
+                tiempoSinInput = 0;
+                if (cinemakina.m_Lens.OrthographicSize < finalsize)
                 {
                     cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + Mathf.Clamp(Mathf.Abs(cc.rb.velocity.y) * indiceMultiplicador * 0.2f * Time.deltaTime, 0, 7);
                 }
@@ -91,28 +138,35 @@ public class CameraZoom : MonoBehaviour
             {
                 if ((cinemakina.m_Lens.OrthographicSize > startsize) && (Mathf.Abs(cc.rb.velocity.y) < 8f) && (Mathf.Abs(cc.rb.velocity.y) > 0.0f))
                 {
-                tiempoSinInput += Time.deltaTime*0.5f;
+                    tiempoSinInput += Time.deltaTime * 0.5f;
                     cinemakina.m_Lens.OrthographicSize -= (Time.deltaTime + tiempoSinInput);
-            }
+                }
                 else
                 {
                     if (cinemakina.m_Lens.OrthographicSize > startsize)
-                {
-                    tiempoSinInput += Time.deltaTime*0.5f;
-                    cinemakina.m_Lens.OrthographicSize -=(Time.deltaTime+tiempoSinInput) ;
+                    {
+                        tiempoSinInput += Time.deltaTime * 0.5f;
+                        cinemakina.m_Lens.OrthographicSize -= (Time.deltaTime + tiempoSinInput);
                     }
                 }
             }
+        }
+        else
+        {
+            maxDistance = DistanciaMaxima();
+            if (cinemakina.m_Lens.OrthographicSize < maxDistance * 0.6f && cinemakina.m_Lens.OrthographicSize < maxZoom)
+            {
+                cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize + indiceMultiplicadorZoom * Time.deltaTime;
+            }
+        }
+        //      }
+        //      else
+        //      {
+        //          if (cinemakina.m_Lens.OrthographicSize > 2)
+        //          {
+        //cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize -= Time.deltaTime * 10;
+        //          }
 
-
-  //      }
-  //      else
-  //      {
-  //          if (cinemakina.m_Lens.OrthographicSize > 2)
-  //          {
-  //cinemakina.m_Lens.OrthographicSize = cinemakina.m_Lens.OrthographicSize -= Time.deltaTime * 10;
-  //          }
-          
-  //      }
+        //      }
     }
 }
