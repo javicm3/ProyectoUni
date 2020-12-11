7a45afe0dadc07de04498bdaf83a640c5a3bcf68
javicm3 <javier.muriel@live.u-tad.com>
2020-12-11 20:15:21 +0100

    EnemigoVolador
    Enemigo que vuela que se mueve por puntos que hay en la escena recorriendo un array.

---
+++ Assets/Scripts/ScriptJavi/EnemigoVolador.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoVolador : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Transform[] puntosPersonaje;
+    int indexArray;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        indexArray = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(indexArray);
+        print(puntosPersonaje.Length);      
+        if (Vector3.Distance(this.transform.position, puntosPersonaje[indexArray].position) > 1)
+        {
+             this.transform.Translate((puntosPersonaje[indexArray].position - this.transform.position).normalized * Time.deltaTime * speed); 
+        }
+        else
+        {
+            if(indexArray < puntosPersonaje.Length - 1 )
+            {
+                indexArray++;                
+            }
+            else
+            {
+                indexArray = 0;                
+            }
+        }
+    }
+}
