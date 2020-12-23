3e145d6949ac9f3b48797a376059bfdfca224db3
ritamonicag <ritamogar@gmail.com>
2020-12-23 21:37:09 +0100

    Luces naves
    Estan hechas las luces de policia y las luces de la limusina

---
+++ Assets/Arte/Sprites/Elementos escenario/TrenyNave/LucesPolicia.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LucesPolicia : MonoBehaviour
+{
+    [SerializeField] GameObject colorRojo;
+    [SerializeField] GameObject colorAzul;
+    bool colorRojoBool; 
+    [SerializeField]float timing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colorRojo.SetActive(true);
+        colorAzul.SetActive(false);
+        colorRojoBool = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timing += 1 + Time.deltaTime;
+        if (timing > 25)
+        {
+            if (colorRojoBool)
+            {
+                colorRojo.SetActive(false);
+                colorAzul.SetActive(true);
+                colorRojoBool = false;
+            }
+            else if(!colorRojoBool)
+            {
+                colorRojo.SetActive(true);
+                colorAzul.SetActive(false);
+                colorRojoBool = true;
+            }
+            timing = 0; 
+            
+            
+            //if (timing > 6)
+            //{
+            //    timing = 0;
+            //    colorRojo.SetActive(true);
+            //    colorAzul.SetActive(false);
+            //}
+        }
+    }
+}
