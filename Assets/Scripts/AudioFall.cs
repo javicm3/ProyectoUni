e7130a668b68810d36a628262af20898bf528979
javicm3 <javier.muriel@live.u-tad.com>
2020-12-30 20:22:51 +0100

    CambiosMenores

--- Assets/Scripts/AudioFall.cs
+++ Assets/Scripts/AudioFall.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class AudioFall : MonoBehaviour
 {
-    bool isGrounded;
+    //bool isGrounded;
 
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Entered");
-        if (collision.gameObject.layer == 8)
-        {
-            isGrounded = true;
-        }
-    }
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("Entered");
+    //    if (collision.gameObject.layer == 8)
+    //    {
+    //        isGrounded = true;
+    //    }
+    //}
 
-    void OnCollisionExit(Collision collision)
-    {
-        Debug.Log("Exited");
-        if (collision.gameObject.layer == 8)
-        {
-            isGrounded = false;
-        }
-    }
+    //void OnCollisionExit(Collision collision)
+    //{
+    //    Debug.Log("Exited");
+    //    if (collision.gameObject.layer == 8)
+    //    {
+    //        isGrounded = false;
+    //    }
+    //}
 }
