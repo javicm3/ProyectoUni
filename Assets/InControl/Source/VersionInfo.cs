20e379b5843842de4316015f4aea886e5038ba07
ritamonicag <ritamogar@gmail.com>
2021-01-01 21:05:55 +0100

    Revert "Vistiendo nivel 2"
    This reverts commit 8ddab7f4f70f3c7f94ef474ff9648dad92148556.

--- Assets/InControl/Source/VersionInfo.cs
+++ Assets/InControl/Source/VersionInfo.cs
@@ -28,11 +28,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 4,
-<<<<<<< HEAD
-				Build = 5487
-=======
-				Build = 5481
->>>>>>> a9aad2cdf29c9ccb4670ec0b11195494382dcb58
+				Build = 5439
 			};
 		}
 
