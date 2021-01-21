53672f044540aaaa5334d3f4ba32138b37f3c7fc
ritamonicag <rita.garcia@live.u-tad.com>
2021-01-21 17:17:54 +0100

    Trailer

---
+++ Assets/Arte/ArteParaTrailer/CambioTanque.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioTanque : MonoBehaviour
+{
+    [SerializeField] GameObject TanqueNormal; 
+    [SerializeField] GameObject TanqueRoto;
+    float timer = 0; 
+    [SerializeField]float tiempoDeCambio = 0;
+    [SerializeField] Animator NucleoAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TanqueNormal.SetActive(true);
+        TanqueRoto.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 1 * Time.deltaTime;
+        if (timer > tiempoDeCambio)
+        {
+            TanqueNormal.SetActive(false);
+            TanqueRoto.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Arte/ArteParaTrailer/DesaparecerPersonaje.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesaparecerPersonaje : MonoBehaviour
+{
+    float timer = 0;
+    [SerializeField] float tiempoDeCambio;
+    public GameObject cuerpo;
+    [SerializeField] Transform puntoOrdenador;
+    [SerializeField] Animator PersonajeTrailerAnim;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        cuerpo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > tiempoDeCambio)
+        {
+
+            cuerpo.SetActive(true);
+        }
+        if (timer > 15.5f)
+        {
+            if(Vector2.Distance(transform.position, puntoOrdenador.position) >.5f)
+            this.gameObject.transform.Translate(new Vector3 (puntoOrdenador.position.x,0,0) * 0.08f * Time.deltaTime);
+        }
+        if (/*transform.position == new Vector3(puntoOrdenador.position.x, transform.position.y, transform.position.z)*/ timer > 31f)
+        {
+            PersonajeTrailerAnim.SetTrigger("Idle");
+        }
+    }
+}
