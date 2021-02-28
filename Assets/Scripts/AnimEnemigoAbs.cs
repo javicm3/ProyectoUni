df05c37c87e7dc4f9487d3d13b02b4eaf1e1ddec
Paula_Gol <p.golosina@gmail.com>
2021-02-28 13:22:27 +0100

    Animacion Enemigo

---
+++ Assets/Scripts/AnimEnemigoAbs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEnemigoAbs : MonoBehaviour
+{
+    EnemigoAbsorb enemigo;
+
+    void Start()
+    {
+        enemigo = GetComponentInParent<EnemigoAbsorb>();
+    }
+
+    public void EventoAtacar()
+    {
+        enemigo.EjecutarAtaque();
+    }
+}
