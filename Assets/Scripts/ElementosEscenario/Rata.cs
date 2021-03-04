e886f43b05c28de402d37b664dcbdb05a98d3a13
EsperanzaBarcia <espe1908@gmail.com>
2021-03-04 17:10:14 +0100

    Cambios nivel 2

---
+++ Assets/Scripts/ElementosEscenario/Rata.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Rata : MonoBehaviour
+{
+    Transform posicionInicial;
+    [SerializeField] float speed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionInicial = this.transform; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+      
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.transform.position = posicionInicial.position; 
+    }
+}
