51027b4f94838a7592d8db262d3955fd2846dadb
Isoth <55432207+Isoth@users.noreply.github.com>
2021-03-05 19:28:02 +0100

    RGEERG

--- Assets/zOtros/PruebasNuevoMovimiento/Scripts/ControllerPersonaje.cs
+++ Assets/zOtros/PruebasNuevoMovimiento/Scripts/ControllerPersonaje.cs
@@ -2789,7 +2789,7 @@ public class ControllerPersonaje : MonoBehaviour
                     {
                         if (tiempoCOYOTE < -0.05f && mEnergy.actualEnergy > mEnergy.energiaDashAbajo)
                         {
-                            mEnergy.RestarEnergia(30);
+                            mEnergy.RestarEnergia(mEnergy.energiaDashAbajo);
                             rb.AddForce(fuerzaDashCaida * Vector2.down/* * Time.deltaTime*/);
                             FindObjectOfType<NewAudioManager>().Play("PlayerDashFall");
                             //this.GetComponent<AudioManager>().Play(this.GetComponent<AudioManager>().sonidosUnaVez, this.GetComponent<AudioManager>().dashAbajo);
