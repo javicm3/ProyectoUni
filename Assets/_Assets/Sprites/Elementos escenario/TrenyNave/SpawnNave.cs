0e62b1cee83698a3fecf48c7dc952904462fa317
Isoth <55432207+Isoth@users.noreply.github.com>
2021-03-05 21:30:36 +0100

    adghtrgb

--- Assets/_Assets/Sprites/Elementos escenario/TrenyNave/SpawnNave.cs
+++ Assets/_Assets/Sprites/Elementos escenario/TrenyNave/SpawnNave.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 public class SpawnNave : MonoBehaviour
 {
     public GameObject nave;
-    float tmp;
-    public float tiempoSpawn = 5;
+    public float tmpMin;
+    public float tmpMax;
+    float tiempoSpawn = 5;
     float posicion;
     public float rango;
-    // Start is called befor e the first frame update
-    void Start()
-    {
-        tmp = tiempoSpawn;
-    }
 
-    // Update is called once per frame
     void Update()
     {
         posicion = Random.Range(this.transform.position.y + rango, this.transform.position.y - rango);
@@ -25,7 +20,7 @@ public class SpawnNave : MonoBehaviour
         {
             Instantiate(nave, new Vector3(transform.position.x, posicion), this.transform.rotation);
 
-            tiempoSpawn = tmp;
+            tiempoSpawn = Random.Range(tmpMin, tmpMax);
 
         }
     }
