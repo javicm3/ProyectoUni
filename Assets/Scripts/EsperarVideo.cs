865a5908de2f1a3bbf41adc682cbdeb92093e614
Paula_Gol <p.golosina@gmail.com>
2021-03-17 00:58:03 +0100

    Pantalla del logo
    Hay que cambiarla por un video más largo y testearla en la build

---
+++ Assets/Scripts/EsperarVideo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EsperarVideo : MonoBehaviour
+{
+    [SerializeField] float tiempoEspera = 3;
+
+    void Start()
+    {
+        StartCoroutine(Wait(tiempoEspera));
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(1);
+    }
+}
