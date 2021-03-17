f93029657be707365efe12fc9e6d38994e30d234
Isoth <55432207+Isoth@users.noreply.github.com>
2021-03-17 12:16:38 +0100

    fwafwa

---
+++ Assets/Camara.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+
+    public GameObject m_camara;
+    public GameObject m_nuevaCamara;
+
+    public bool m_cambio;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if(m_cambio == true)
+            {
+                m_camara.SetActive(false);
+                m_nuevaCamara.SetActive(true);
+            }
+            if (m_cambio == false)
+            {
+                m_camara.SetActive(true);
+                m_nuevaCamara.SetActive(false);
+            }
+        }
+    }
+}
