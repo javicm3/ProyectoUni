912e7d48beb103386a5f50c0d0a5e269eb4bf08b
JorgeUtad <jorge.padron@live.u-tad.com>
2021-03-20 18:45:11 +0100

    cambio codigo personaje trailer

--- Assets/_Assets/ArteParaTrailer/DesaparecerPersonaje.cs
+++ Assets/_Assets/ArteParaTrailer/DesaparecerPersonaje.cs
@@ -8,29 +8,39 @@ public class DesaparecerPersonaje : MonoBehaviour
     [SerializeField] float tiempoDeCambio;
     public GameObject cuerpo;
     [SerializeField] Transform puntoOrdenador;
-    [SerializeField] Animator PersonajeTrailerAnim;  
+    [SerializeField] Animator PersonajeTrailerAnim;
+    bool caminar;
     // Start is called before the first frame update
     void Start()
     {
         cuerpo.SetActive(false);
+        caminar = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 punt = new Vector3(puntoOrdenador.position.x, transform.position.y, 0);
         timer += Time.deltaTime;
         if (timer > tiempoDeCambio)
         {
 
             cuerpo.SetActive(true);
         }
-        if (timer > 15f)
+        if (timer > 2f && caminar)
         {
-            if(Vector2.Distance(transform.position, puntoOrdenador.position) >.5f)
-            this.gameObject.transform.Translate(new Vector3 (puntoOrdenador.position.x,0,0) * 0.3f * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, punt, 2f * Time.deltaTime);
+
+¡            //if (Vector2.Distance(transform.position, puntoOrdenador.position) > .5f)
+            //{
+            //    this.transform.position = Vector3.MoveTowards(transform.position, new Vector3(puntoOrdenador.position.x, transform.position.y, 0), 2.5f * Time.deltaTime);
+            //    //this.gameObject.transform.Translate(new Vector3(puntoOrdenador.position.x, 0, 0) * 0.3f * Time.deltaTime);
+            //}
         }
-        if (/*transform.position == new Vector3(puntoOrdenador.position.x, transform.position.y, transform.position.z)*/ timer > 19.5f)
+        if (/*transform.position == new Vector3(puntoOrdenador.position.x, transform.position.y, transform.position.z)*/ timer > 7.8f && caminar)
         {
+            caminar = false;
             PersonajeTrailerAnim.SetTrigger("Idle");
         }
     }
