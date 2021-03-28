6b1d879c45c08f4768655e3512608c77f9fde316
ArtemAnk1 <33345147+ArtemAnk1@users.noreply.github.com>
2021-03-28 21:13:01 +0200

    feedback combate

---
+++ Assets/_Assets/Enemigos/EfectosEnemigos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EfectosEnemigos : MonoBehaviour
+{
+    public GameObject particulasDaño;
+    public Transform puntoDaño;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ParticulasDañado()
+    {
+        Instantiate(particulasDaño, puntoDaño);
+    }
+}
