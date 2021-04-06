cced4759ababa7ccd4e4aa650eba061eb6cda08f
javicm3 <javier.muriel@live.u-tad.com>
2021-04-06 19:20:18 +0200

    Coleccionables ND7

---
+++ Assets/Scripts/CódidoNoJulio/ContadorColeccionables.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContadorColeccionables : MonoBehaviour
+{
+    public float nColeccionables;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nColeccionables = GameObject.FindGameObjectsWithTag("Coleccionable").Length;
+        print(nColeccionables);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
