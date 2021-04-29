c681fd599e5419199cb7b4e477babba56124644f
ArtemAnk1 <33345147+ArtemAnk1@users.noreply.github.com>
2021-04-29 21:40:38 +0200

    Vistiendo nivel 3

---
+++ Assets/Scripts/Boss2/senalgiratoria.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class senalgiratoria : MonoBehaviour
+{
+    public float velocidadMov;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        transform.RotateAround(transform.position, transform.forward, velocidadMov * Time.deltaTime);
+    }
+
+}
