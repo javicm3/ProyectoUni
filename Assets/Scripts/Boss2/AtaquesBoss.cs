d686b7a59fd8c9b0b574a724240a8bb1d3760942
ArtemAnk1 <33345147+ArtemAnk1@users.noreply.github.com>
2021-05-20 21:12:47 +0200

    correcciones boss final

--- Assets/Scripts/Boss2/AtaquesBoss.cs
+++ Assets/Scripts/Boss2/AtaquesBoss.cs
@@ -126,8 +126,9 @@ public class AtaquesBoss : MonoBehaviour
         
         if (vert1 != null || vert2 != null)
         {
-            vert1.transform.position = Vector3.MoveTowards(vert1.transform.position, posRayoVert2.position, speedRayosVert);
-            vert2.transform.position = Vector3.MoveTowards(vert2.transform.position, posRayoVert1.position, speedRayosVert);
+            print(Time.deltaTime);
+            vert1.transform.position = Vector3.MoveTowards(vert1.transform.position, posRayoVert2.position, speedRayosVert * Time.deltaTime);
+            vert2.transform.position = Vector3.MoveTowards(vert2.transform.position, posRayoVert1.position, speedRayosVert * Time.deltaTime);
             //if (vert1.transform.position == posRayoVert2.position)
             //{               
             //    eb.ataqueTerminado = true;
