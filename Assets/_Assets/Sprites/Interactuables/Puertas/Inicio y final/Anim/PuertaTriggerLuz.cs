fd5e3fbf5121b76fb05a6f30a636ea835a52587c
JorgeUtad <jorge.padron@live.u-tad.com>
2021-05-20 12:00:21 +0200

    cambios animacion puertas inicio y final y modificaciones de posicion de puertas en ND3 y 5

---
+++ Assets/_Assets/Sprites/Interactuables/Puertas/Inicio y final/Anim/PuertaTriggerLuz.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaTriggerLuz : MonoBehaviour
+{
+    Animator animCC;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animCC = GetComponent<Animator>();
+        animCC.SetBool("Player", false);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            animCC.SetBool("Player", true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            animCC.SetBool("Player", false);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            animCC.SetBool("Player", true);
+        }
+    }
+
+}
